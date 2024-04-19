71626a09f49c94b7a80e46ca8860d56e66aabc34
Uri Shaked <uri@urishaked.com>
2024-04-19 20:46:57 +0300

    TT07 Verilog Project Template

--- test/tb.v
+++ test/tb.v
@@ -6,7 +6,7 @@
 */
 module tb ();
 
-  // Dump the signals to a VCD file. You can view it with gtkwave or surfer.
+  // Dump the signals to a VCD file. You can view it with gtkwave.
   initial begin
     $dumpfile("tb.vcd");
     $dumpvars(0, tb);
@@ -22,20 +22,14 @@ module tb ();
   wire [7:0] uo_out;
   wire [7:0] uio_out;
   wire [7:0] uio_oe;
-  wire [6:0] segments = uo_out [6:0];
 
-`ifdef GL_TEST
-  wire VPWR = 1'b1;
-  wire VGND = 1'b0;
-`endif
-
-  // Replace tt_um_factory_test with your module name:
-  tt_um_seven_segment_seconds tt_um_seven_segment_seconds (
+  // Replace tt_um_example with your module name:
+  tt_um_example user_project (
 
       // Include power ports for the Gate Level test:
 `ifdef GL_TEST
-      .VPWR(VPWR),
-      .VGND(VGND),
+      .VPWR(1'b1),
+      .VGND(1'b0),
 `endif
 
       .ui_in  (ui_in),    // Dedicated inputs
